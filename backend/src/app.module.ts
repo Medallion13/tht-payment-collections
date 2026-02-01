caad20d3d033339ea0315072a7933a5d4aba07cf
Medallion13 <nickcar13@gmail.com>
2026-02-01 13:50:25 -0500

    feat(env): Configuration of env variables making accessable for all project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
