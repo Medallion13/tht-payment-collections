4fbf09b504b7d2f4cca099df01884b95709c5758
Medallion13 <nickcar13@gmail.com>
2026-01-31 19:48:45 -0500

    wip(front): adding the mock user data page

---
+++ frontend/src/pages/UserDataPage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function UserDataPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Show transaction end information</h1>
+      <p>Missing form to show info TODO</p>
+      <Link to="/">back</Link>
+    </div>
+  );
+}
